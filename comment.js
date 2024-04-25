9fdf05589fea435e6a12b5a0000823de2d13745a
NathanK <145985819+Nathan-Kerstens@users.noreply.github.com>
2024-04-25 14:47:08 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,23 +1,43 @@
-//Create a web server that listens on port 3000 and serves a file called comment.html that displays the comments from the data.json file.
-//If the file does not exist, the server should respond with a 404 error.
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, 'comment.html');
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('<h1>404 Not Found</h1>');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
+app.get('/comment', function (req, res) {
+    fs.readFile(__dirname + "/" + "comment.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
     });
-});
+})
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+app.post('/comment', function (req, res) {
+    fs.readFile(__dirname + "/" + "comment.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        data.push(req.body);
+        fs.writeFile(__dirname + "/" + "comment.json", JSON.stringify(data), function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        res.end(JSON.stringify(data));
+    });
+})
+
+app.delete('/comment', function (req, res) {
+    fs.writeFile(__dirname + "/" + "comment.json", '[]', function (err) {
+        if (err) {
+            console.log(err);
+        }
+        res.end('[]');
+    });
+})
+
+var server = app.listen(8001, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server is running at http://%s:%s", host, port);
+})
