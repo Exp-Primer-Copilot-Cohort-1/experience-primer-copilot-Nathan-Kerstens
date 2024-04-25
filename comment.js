2b90cb708ff10e8e2f88ddf68164db63c15baf28
NathanK <145985819+Nathan-Kerstens@users.noreply.github.com>
2024-04-25 14:43:01 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,85 +1,23 @@
-// Create web server and listen on port 3000
-// Using Express to handle HTTP requests
-// Using Mongoose to handle MongoDB operations
+//Create a web server that listens on port 3000 and serves a file called comment.html that displays the comments from the data.json file.
+//If the file does not exist, the server should respond with a 404 error.
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Comment = require('./models/comment');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const app = express();
-
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/comment');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'comment.html');
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<h1>404 Not Found</h1>');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
 });
 
-// Create a new comment
-app.post('/comment', (req, res) => {
-  const comment = new Comment(req.body);
-  comment.save((err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(comment);
-    }
-  });
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
-
-// Get all comments
-app.get('/comment', (req, res) => {
-  Comment.find({}, (err, comments) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(comments);
-    }
-  });
-});
-
-// Get a comment by id
-app.get('/comment/:id', (req, res) => {
-  Comment.findById(req.params.id, (err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(comment);
-    }
-  });
-});
-
-// Update a comment by id
-app.put('/comment/:id', (req, res) => {
-  Comment.findByIdAndUpdate(req.params.id, req.body, (err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(comment);
-    }
-  });
-});
-
-// Delete a comment by id
-app.delete('/comment/:id', (req, res) => {
-  Comment.findByIdAndRemove(req.params.id, (err, comment) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(comment);
-    }
-  });
-});
-
-// Start server
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
-
-// Export app for testing
-module.exports = app;
-
